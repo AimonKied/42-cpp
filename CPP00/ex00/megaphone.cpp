0efc4f8ed4362d5e2d957280ab33a2a0c61e358c
Simon Wied <swied@2-C-5.42heilbronn.de>
2025-10-06 15:56:45 +0200

    NORM(master): removed namespace::std

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,31 +6,30 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 15:42:33 by swied             #+#    #+#             */
-/*   Updated: 2025/10/01 15:46:56 by swied            ###   ########.fr       */
+/*   Updated: 2025/10/06 15:54:18 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main(int argc, char **argv)
 {
 	if (argc == 1)
 	{
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	for (int i = 1; i < argc; i++)
 	{
-		string arg = argv[i];
+		std::string arg = argv[i];
 		for (int j = 0; j < (int)arg.length(); j++)
 		{
-			cout << (char)toupper(arg[j]);
+			std::cout << (char)toupper(arg[j]);
 		}
 		if (i < argc -1)
-			cout << " ";
+			std::cout << " ";
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
