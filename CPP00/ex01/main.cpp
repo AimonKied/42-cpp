b8f766f3889d726afc4b24280530500c6928bf28
Simon Wied <swied@2-B-7.42heilbronn.de>
2025-10-09 16:51:09 +0200

    FIX(master): If getline has no input (ctrl-d) it quits, if wrong command (also enter) it shows message

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,38 +6,39 @@
 /*   By: swied <swied@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 21:42:45 by swied             #+#    #+#             */
-/*   Updated: 2025/10/08 12:43:14 by swied            ###   ########.fr       */
+/*   Updated: 2025/10/09 16:23:06 by swied            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
 int main() {
-    PhoneBook phonebook;
-    std::string command;
-    
-    std::cout << "Welcome to your PhoneBook!" << std::endl;
-    std::cout << "Available commands: ADD, SEARCH, EXIT" << std::endl;
-    
-    while (true) {
-        std::cout << "Enter command: ";
-        std::getline(std::cin, command);
-        
-        if (command == "ADD") {
-            phonebook.addContact();
-        }
-        else if (command == "SEARCH") {
-            phonebook.searchContact();
-        }
-        else if (command == "EXIT") {
-            std::cout << "Goodbye!" << std::endl;
-            break;
-        }
-        else {
-            std::cout << "Unknown command. Available commands: ADD, SEARCH, EXIT" << std::endl;
-        }
-    }
-    
-    return 0;
-}
+	PhoneBook phonebook;
+	std::string command;
 
+	std::cout << "Welcome to your PhoneBook!" << std::endl;
+	std::cout << "Available commands: ADD, SEARCH, EXIT" << std::endl;
+
+	while (true) {
+		std::cout << "Enter command: ";
+		if (!std::getline(std::cin, command)) {
+			std::cout << std::endl << "Goodbye!" << std::endl;
+			break;
+		}
+		
+		if (command == "ADD") {
+			phonebook.addContact();
+		}
+		else if (command == "SEARCH") {
+			phonebook.searchContact();
+		}
+		else if (command == "EXIT") {
+			std::cout << "Goodbye!" << std::endl;
+			break;
+		}
+		else {
+			std::cout << "Unknown command. Available commands: ADD, SEARCH, EXIT" << std::endl;
+		}
+	}
+	return 0;
+}
